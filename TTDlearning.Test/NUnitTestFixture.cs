fe2b6ec440d3c39bcd4a6e9021bece7ec85adf6c
kaziz febrinata' <kaziz.febrinata@gmail.com>
2014-03-20 11:51:17 +0700

    +add ttd learning project
    test driven dev using c# and nunit

---
+++ TTDlearning.Test/NUnitTestFixture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TTDlearning;
+
+
+namespace TTDlearning.Test
+{
+    [TestFixture]
+    public class NUnitTestFixture
+    {
+        TDDlearning.MathsHelper TDD = null;
+
+        [SetUp]
+        public void Init()
+        {
+            TDD = new TDDlearning.MathsHelper();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            TDD = null;
+        }
+
+        [TestCase]
+        public void AddTest()
+        {
+            int result = TDD.Add(20, 10);
+            Assert.AreEqual(30, result);
+        }
+        [TestCase]
+        public void SubtractTest()
+        {
+            int result = TDD.Subtract(20, 10);
+            Assert.AreEqual(10, result);
+        }
+
+        [TestCase]
+        public void DevideTest()
+        {
+            int result = TDD.devide(4, 2);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
+
